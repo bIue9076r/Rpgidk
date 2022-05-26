59e9faf380e6585850ce51d3f34e0e8361b9e4b0
blue <nobfi517@gmail.com>
2022-05-25 21:07:49 -0500

    legacy branch test 1

--- main.lua
+++ main.lua
@@ -1,3 +1,3 @@
 function love.load() require("/Required/Game"); Game.load() end
 function love.update(dt) Game.update(dt) end
-function love.draw() Game.draw() end
+function love.draw() Game.draw()  end
